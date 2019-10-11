914d5368009bddd0be26317e7ec4f2050b3e894c
picada <32310572+picada@users.noreply.github.com>
2019-10-11 21:24:01 +0300

    some basic test code

--- robotuprising2019/main.py
+++ robotuprising2019/main.py
@@ -14,8 +14,8 @@ brick.sound.beep()
 # Initialize a motor at port 
 B.test_motor = Motor(Port.B)
 
-# Run the motor up to 500 degrees
-test_motor.run_target(500, 90) per second. To a target angle of 90 degrees.
+# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+test_motor.run_target(500, 90) 
 
 # Play another beep sound.
 # This time with a higher pitch (1000 Hz) and longer duration (500 ms).
