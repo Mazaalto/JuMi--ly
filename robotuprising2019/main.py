c9c865236fca643342e8285fa31b071821d29458
picada <32310572+picada@users.noreply.github.com>
2019-10-11 21:21:17 +0300

    some basic test code

--- robotuprising2019/main.py
+++ robotuprising2019/main.py
@@ -8,5 +8,19 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
-# Write your program here
+# Play a sound
 brick.sound.beep()
+
+# Initialize a motor at port 
+B.test_motor = Motor(Port.B)
+
+# Run the motor up to 500 degrees
+test_motor.run_target(500, 90) per second. To a target angle of 90 degrees.
+
+# Play another beep sound.
+# This time with a higher pitch (1000 Hz) and longer duration (500 ms).
+brick.sound.beep(1000, 500)
+
+
+
+
