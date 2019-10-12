420289aad1a724688013bc836fb1414d5031a2db
picada <32310572+picada@users.noreply.github.com>
2019-10-12 12:50:37 +0300

    fixed color sensor code

--- robotuprising2019/main.py
+++ robotuprising2019/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-import time
 from pybricks import ev3brick as brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -10,21 +9,20 @@ from pybricks.robotics import DriveBase
 
 import time
 
+
 Vmoottori = Motor(Port.B, Direction.CLOCKWISE)
 Omoottori = Motor(Port.C, Direction.CLOCKWISE)
 
-# Play a sound
-#brick.sound.file("Taistelujaska.wav")
 variSensori = ColorSensor(Port.S2)
 
 # Play a sound
 #brick.sound.file("Taistelujaska.wav")
 i = 0
 while i < 50:
-    vari = variSensori.Color()
+    vari = variSensori.color()
     print(vari)
-    i++
-    time.sleep(0.300)or at port
+    i += 1
+    time.sleep(0.300) 
 #B.test_motor = Motor(Port.B)
 
 # Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
@@ -37,3 +35,4 @@ while i < 50:
 # Low battery warning
 if brick.battery.voltage() < 7000:
     brick.sound.beep()
+
