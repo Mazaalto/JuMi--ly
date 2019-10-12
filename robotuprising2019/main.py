cad0c7d4bde7729c5baaccf2db5f8e30688c541f
picada <32310572+picada@users.noreply.github.com>
2019-10-12 10:22:33 +0300

    first steps

--- robotuprising2019/main.py
+++ robotuprising2019/main.py
@@ -8,24 +8,35 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
-
+irSensor = UltrasonicSensor(Port.S1)
+print(irSensor.distance())
 
 Vmoottori = Motor(Port.B, Direction.CLOCKWISE)
 Omoottori = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 
 # Play a sound
 brick.sound.file("Taistelujaska.wav)
+time.sleep(2)
+
+Vmoottori.run(100)
+Omoottori.run(100)
+time.sleep(4)
+
+Vmoottori.stop()
+Omoottori.stop()
+
+Vmoottori.run(-100)
+Omoottori.run(100)
 time.sleep(1)
 
-Vmoottori.run_time(300, 2000)
-Omoottori.run_time(300, 2000)
+Vmoottori.stop()
+Omoottori.stop()
 # Initialize a motor at port
 #B.test_motor = Motor(Port.B)
-time.sleep(1)
+
 # Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
 #test_motor.run_target(500, 90)
-Vmoottori.run_time(-300, 1000)
-Omoottori.run_time(300, 1000)
+
 # Play another beep sound.
 # This time with a higher pitch (1000 Hz) and longer duration (500 ms).
 #brick.sound.beep(1000, 500)
